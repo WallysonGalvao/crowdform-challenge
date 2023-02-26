5a88f7da02908c30e1bc0c0f60a3857bed6994a1
Wallyson Galvão <wallyson.galvao@sevenapps.com.br>
2023-02-26 11:07:00 -0300

    feat: fonts

--- App.tsx
+++ App.tsx
@@ -2,10 +2,24 @@ import { theme } from "@app/styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Sora_400Regular,
+  Sora_600SemiBold,
+} from "@expo-google-fonts/sora";
 
 import { PublicRoutes } from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: Sora_400Regular,
+    semiBold: Sora_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <ThemeProvider theme={theme}>
